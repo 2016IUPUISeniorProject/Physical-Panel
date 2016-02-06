6021941c596ded4b9c7409275993f59f86ec6956
Gary <garykoers@gmail.com>
2016-02-06 15:36:27 -0500

    da

--- Box4.ino
+++ Box4.ino
@@ -17,6 +17,24 @@
 #include <limits.h>
 #include <string>
 
+///////IMU SETUP
+LSM9DS1 imu;                // lsm9d1 is a class from the library....
+// SDO_XM and SDO_G are both pulled high, so our addresses are:
+#define LSM9DS1_M 0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
+// http://www.ngdc.noaa.gov/geomag-web/#declination
+#define DECLINATION 4.76 // Declination (degrees) in Boulder, CO.
+unsigned long startTime;
+unsigned int accelReadCounter = 0;
+unsigned int gyroReadCounter = 0;
+unsigned int magReadCounter = 0;
+unsigned int tempReadCounter = 0;
+unsigned long lastPrint = 0;
+const unsigned int PRINT_RATE = 500;
+//////END IMU SETUP
+
+
+
 //SYSTEM_MODE(MANUAL);  //set this if using websockets (requires changing)
 
 ////////////////////////////////////////////////////////////////
@@ -28,6 +46,7 @@ void CHECKENCODERCHANGED();
 void doEncoderA();
 // Interrupt on B changing state, same as A above
 void doEncoderB();
+//int PREVIOUS_ENCODERLEDSTATUS;  //to replace prevPos
 //////////////////////////////////////////////////////////////////
 
 
@@ -61,6 +80,8 @@ int ENCODERB=A2;
 int prevPos = 0;
 int PREVIOUS_ENCODERLEDSTATUS;  //to replace prevPos
 int value = 0;
+int PREVIOUS_ENCODERA_STATUS;
+int PREVIOUS_ENCODERB_STATUS;
 //////////END ROTARY ENCODER////////////
 int REDBUTTONSTATUS=LOW;
 int BLUEBUTTONSTATUS=LOW;
@@ -117,11 +138,11 @@ void setup() {
   strip.show(); // Initialize all pixels to 'off'
   strip.setBrightness(233);
   //initialize the led ring to 0, (top, slightly to the right)
-        strip.clear();
-        strip.setPixelColor(encoderPos, 0, 255, 255);
-        strip.show();
+  strip.clear();
+  strip.setPixelColor(encoderPos, 0, 255, 255);
+  strip.show();
   
-  //attachInterrupt(ENCODERA, doEncoderA, CHANGE);
+  //attachInterrupt(ENCODERA, doEncoderA, CHANGE);               // previously used interrupts, but caused issues with neopixel
   //attachInterrupt(ENCODERB, doEncoderB, CHANGE);  
     
   pinMode(REDLED, OUTPUT);
@@ -137,6 +158,17 @@ void setup() {
   pinMode(BLUEBUTTON,INPUT_PULLUP);
   pinMode(GREENBUTTON,INPUT_PULLUP);
   pinMode(YELLOWBUTTON,INPUT_PULLUP);
+
+  // imu setup
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  
+// rotary 
+PREVIOUS_ENCODERA_STATUS=digitalRead(ENCODERA);
+PREVIOUS_ENCODERB_STATUS=digitalRead(ENCODERB);
+
+
 }//end setup
 
 
@@ -214,22 +246,41 @@ if((BUTTONPRESSED==1))//&&(WAITFORRELEASE==0))          //button pressed for fir
     }
 
 
-// if((WAITFORRELEASE==1)&&(BUTTONPRESSED==0))                           //if waiting for release and release happened
- //           {
-  //             WAITFORRELEASE=0;
-//            }
 
-//}
-
-//check if virtual world has changed
 if(Serial.available()>0)
     {
     COMMUNICATE();
     }
     
-    
-    //UPDATELIGHTS();
-    //COMMUNICATE();
+      // imu.accelAvailable() returns 1 if new accelerometer
+  // data is ready to be read. 0 otherwise.
+  if (imu.accelAvailable())
+  {
+    imu.readAccel();
+    accelReadCounter++;
+  }
+  // imu.gyroAvailable() returns 1 if new gyroscope
+  // data is ready to be read. 0 otherwise.
+  if (imu.gyroAvailable())
+  {
+    imu.readGyro();
+    gyroReadCounter++;
+  }
+  // imu.magAvailable() returns 1 if new magnetometer
+  // data is ready to be read. 0 otherwise.
+  if (imu.magAvailable())
+  {
+    imu.readMag();
+    magReadCounter++;
+  }
+  // Every PRINT_RATE milliseconds, print sensor data:
+  if ((lastPrint + PRINT_RATE) < millis())
+  {
+    printSensorReadings();
+    lastPrint = millis();
+  }
+
+
 }//end loop/////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -461,4 +512,155 @@ void doEncoderB(){
       ENCODERLEDSTATUS -= 1;
     }
   }
+}
+
+void setupGyro()
+{
+  // [enabled] turns the gyro on or off.
+  imu.settings.gyro.enabled = true;  // Enable the gyro
+  // [scale] sets the full-scale range of the gyroscope.
+  // scale can be set to either 245, 500, or 2000
+  imu.settings.gyro.scale = 245; // Set scale to +/-245dps
+  // [sampleRate] sets the output data rate (ODR) of the gyro
+  // sampleRate can be set between 1-6
+  // 1 = 14.9    4 = 238
+  // 2 = 59.5    5 = 476
+  // 3 = 119     6 = 952
+  imu.settings.gyro.sampleRate = 3; // 59.5Hz ODR
+  // [bandwidth] can set the cutoff frequency of the gyro.
+  // Allowed values: 0-3. Actual value of cutoff frequency
+  // depends on the sample rate. (Datasheet section 7.12)
+  imu.settings.gyro.bandwidth = 0;
+  // [lowPowerEnable] turns low-power mode on or off.
+  imu.settings.gyro.lowPowerEnable = false; // LP mode off
+  // [HPFEnable] enables or disables the high-pass filter
+  imu.settings.gyro.HPFEnable = true; // HPF disabled
+  // [HPFCutoff] sets the HPF cutoff frequency (if enabled)
+  // Allowable values are 0-9. Value depends on ODR.
+  // (Datasheet section 7.14)
+  imu.settings.gyro.HPFCutoff = 1; // HPF cutoff = 4Hz
+  // [flipX], [flipY], and [flipZ] are booleans that can
+  // automatically switch the positive/negative orientation
+  // of the three gyro axes.
+  imu.settings.gyro.flipX = false; // Don't flip X
+  imu.settings.gyro.flipY = false; // Don't flip Y
+  imu.settings.gyro.flipZ = false; // Don't flip Z
+}
+
+void setupAccel()
+{
+  // [enabled] turns the acclerometer on or off.
+  imu.settings.accel.enabled = true; // Enable accelerometer
+  // [enableX], [enableY], and [enableZ] can turn on or off
+  // select axes of the acclerometer.
+  imu.settings.accel.enableX = true; // Enable X
+  imu.settings.accel.enableY = true; // Enable Y
+  imu.settings.accel.enableZ = true; // Enable Z
+  // [scale] sets the full-scale range of the accelerometer.
+  // accel scale can be 2, 4, 8, or 16
+  imu.settings.accel.scale = 8; // Set accel scale to +/-8g.
+  // [sampleRate] sets the output data rate (ODR) of the
+  // accelerometer. ONLY APPLICABLE WHEN THE GYROSCOPE IS
+  // DISABLED! Otherwise accel sample rate = gyro sample rate.
+  // accel sample rate can be 1-6
+  // 1 = 10 Hz    4 = 238 Hz
+  // 2 = 50 Hz    5 = 476 Hz
+  // 3 = 119 Hz   6 = 952 Hz
+  imu.settings.accel.sampleRate = 1; // Set accel to 10Hz.
+  // [bandwidth] sets the anti-aliasing filter bandwidth.
+  // Accel cutoff freqeuncy can be any value between -1 - 3. 
+  // -1 = bandwidth determined by sample rate
+  // 0 = 408 Hz   2 = 105 Hz
+  // 1 = 211 Hz   3 = 50 Hz
+  imu.settings.accel.bandwidth = 0; // BW = 408Hz
+  // [highResEnable] enables or disables high resolution 
+  // mode for the acclerometer.
+  imu.settings.accel.highResEnable = false; // Disable HR
+  // [highResBandwidth] sets the LP cutoff frequency of
+  // the accelerometer if it's in high-res mode.
+  // can be any value between 0-3
+  // LP cutoff is set to a factor of sample rate
+  // 0 = ODR/50    2 = ODR/9
+  // 1 = ODR/100   3 = ODR/400
+  imu.settings.accel.highResBandwidth = 0;  
+}
+
+void setupMag()
+{
+  // [enabled] turns the magnetometer on or off.
+  imu.settings.mag.enabled = true; // Enable magnetometer
+  // [scale] sets the full-scale range of the magnetometer
+  // mag scale can be 4, 8, 12, or 16
+  imu.settings.mag.scale = 12; // Set mag scale to +/-12 Gs
+  // [sampleRate] sets the output data rate (ODR) of the
+  // magnetometer.
+  // mag data rate can be 0-7:
+  // 0 = 0.625 Hz  4 = 10 Hz
+  // 1 = 1.25 Hz   5 = 20 Hz
+  // 2 = 2.5 Hz    6 = 40 Hz
+  // 3 = 5 Hz      7 = 80 Hz
+  imu.settings.mag.sampleRate = 5; // Set OD rate to 20Hz
+  // [tempCompensationEnable] enables or disables 
+  // temperature compensation of the magnetometer.
+  imu.settings.mag.tempCompensationEnable = false;
+  // [XYPerformance] sets the x and y-axis performance of the
+  // magnetometer to either:
+  // 0 = Low power mode      2 = high performance
+  // 1 = medium performance  3 = ultra-high performance
+  imu.settings.mag.XYPerformance = 3; // Ultra-high perform.
+  // [ZPerformance] does the same thing, but only for the z
+  imu.settings.mag.ZPerformance = 3; // Ultra-high perform.
+  // [lowPowerEnable] enables or disables low power mode in
+  // the magnetometer.
+  imu.settings.mag.lowPowerEnable = false;
+  // [operatingMode] sets the operating mode of the
+  // magnetometer. operatingMode can be 0-2:
+  // 0 = continuous conversion
+  // 1 = single-conversion
+  // 2 = power down
+  imu.settings.mag.operatingMode = 0; // Continuous mode
+}
+
+uint16_t initLSM9DS1()
+{
+  setupGyro(); // Set up gyroscope parameters
+  setupAccel(); // Set up accelerometer parameters
+  setupMag(); // Set up magnetometer parameters
+  //setupTemperature(); // Set up temp sensor parameter
+  
+  return imu.begin();
+}
+
+void printSensorReadings()
+{
+  float runTime = (float)(millis() - startTime) / 1000.0;
+  float accelRate = (float)accelReadCounter / runTime;
+  float gyroRate = (float)gyroReadCounter / runTime;
+  float magRate = (float)magReadCounter / runTime;
+
+  Serial.print("A: ");
+  Serial.print(imu.calcAccel(imu.ax));
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.ay));
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.az));
+  Serial.print(" g \t| ");
+
+  Serial.print("G: ");
+  Serial.print(imu.calcGyro(imu.gx));
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gy));
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gz));
+  Serial.print(" dps \t| ");
+
+  Serial.print("M: ");
+  Serial.print(imu.calcMag(imu.mx));
+  Serial.print(", ");
+  Serial.print(imu.calcMag(imu.my));
+  Serial.print(", ");
+  Serial.print(imu.calcMag(imu.mz));
+  Serial.print(" Gs \t| ");
+ 
+  //Serial.println();
 }
